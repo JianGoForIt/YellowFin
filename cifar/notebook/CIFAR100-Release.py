4acc79efd411b2e0a86135282d3f99aa610ad354
Jian Zhang <zhangjianthu@gmail.com>
2017-05-30 20:46:14 -0700

    clean version script done for cifar100

--- cifar/notebook/CIFAR100-Release.py
+++ cifar/notebook/CIFAR100-Release.py
@@ -15,23 +15,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-sys.path.append('../tuner_utils')
-from robust_region_v2 import *
-sys.path.append('../scripts')
-from resnet_utils import *
-
 sys.path.append('../scripts')
 import resnet_model
-from resnet_main import train
+from resnet_utils import *
 import cifar_input
-
-
-# define single step function
-def train_single_step_frame(sess, model_train, lr_val, mom_val, clip_norm_base):      
-    output_results = sess.run( [model_train.cost] + model_train.grads +[model_train.train_op, ] )    
-    loss = output_results[0]
-    grads = output_results[1:(1 + len(model_train.grads) ) ]
-    return grads, loss
 
 
 # set up path and other parameters
@@ -60,53 +47,32 @@ hps_train = resnet_model.HParams(batch_size=batch_size_train,
                          weight_decay_rate=0.0002,
                          relu_leakiness=0.1,
                          optimizer='YF', model_scope='train')
-hps_eval = resnet_model.HParams(batch_size=batch_size_test,
-                         num_classes=NUM_CLASSES,
-                         min_lrn_rate=0.0001,
-                         lrn_rate=0.1,
-                         mom=0.9,
-                         clip_norm_base=10.0,
-                         num_residual_units=5,
-                         use_bottleneck=True,
-                         weight_decay_rate=0.0002,
-                         relu_leakiness=0.1,
-                         optimizer='YF', model_scope='train')
-
+# specify how much memory to use on each GPU
 gpu_mem_portion=0.5
-model_train, model_eval, init_op, mon_sess = setup(hps_train, hps_eval, gpu_mem_portion, DEV, DATASET, TRAIN_DATA_PATH, TEST_DATA_PATH)
+n_core = 16
+with tf.variable_scope("train"), tf.device(DEV):
+        model_train = get_model(hps_train, DATASET, TRAIN_DATA_PATH, mode='train')
+init_op = tf.global_variables_initializer()
+sess = GetTrainingSession(model_train, gpu_mem_portion=gpu_mem_portion)
 
 
-# In[10]:
-
+# run steps
 general_log_dir = "../results"
+log_dir = general_log_dir + "/test-release-cifar100-final"
 num_step = 70001
-display_interval=500
-test_interval=1000 # no test
-clip_norm_base = 100.0
-# the following a few params are all dummy and placeholder
-train_batch_size = 128
-grad_beta = 0.9
-dist_beta = 0.9
-curv_beta = 0.999
-param_beta = 0.999
-sliding_win_width=10
+display_interval=2500
 
-
-# In[12]:
-
-lr_val = 1.0
-mom_val = 0.0
-do_pred = True
-log_dir = general_log_dir + "/test-release-cifar100"
 if not os.path.isdir(log_dir):
     os.mkdir(log_dir)
 
 tf.set_random_seed(1)
-mon_sess.run(init_op)
+sess.run(init_op)
 
-train_single_step = lambda lr_val, mom_val, clip_norm_base: train_single_step_frame(mon_sess, model_train, lr_val, mom_val, clip_norm_base)
-
-loss_list, g_norm_list = train_rave_noisy_acc(mon_sess, model_eval, num_step, lr_val, mom_val, 
-                     clip_norm_base, do_pred, curv_beta, param_beta, train_single_step, sliding_win_width, display_interval, test_interval, log_dir=log_dir)
-np.savetxt(log_dir + "/loss_full.txt", np.array(loss_list) )
-np.savetxt(log_dir + "/g_norm_full.txt", np.array(g_norm_list) )
+loss_list = []
+for i in range(num_step):
+    loss, _ = sess.run( [model_train.cost, model_train.train_op ] )
+    loss_list.append(loss)
+    if (i % display_interval == 0 or i == 50) and (i != 0):
+        print "plotting for iteration ", i
+        plot_loss(loss_list, log_dir, i)
+        np.savetxt(log_dir + "/loss_full.txt", np.array(loss_list) )
--- cifar/scripts/resnet_utils.py
+++ cifar/scripts/resnet_utils.py
@@ -15,10 +15,6 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def running_mean(x, N):
-  cumsum = np.cumsum(np.insert(x, 0, 0)) 
-  return (cumsum[N:] - cumsum[:-N]) / N 
-
 
 def get_model(hps, dataset, train_data_path, mode='train'):
     images, labels = cifar_input.build_input(
@@ -37,118 +33,20 @@ def GetTrainingSession(model_train, n_core=16, gpu_mem_portion=0.99):
     return mon_sess
 
 
-def setup(hps_train, hps_eval, gpu_mem_portion, DEV, DATASET, TRAIN_DATA_PATH, TEST_DATA_PATH):
-    with tf.variable_scope("train"), tf.device(DEV):
-        model_train = get_model(hps_train, DATASET, TRAIN_DATA_PATH, mode='train')
-        
-    # use the train for the scope name just for reuse the variable
-    with tf.variable_scope("train", reuse=True), tf.device(DEV):
-        model_eval = get_model(hps_eval, DATASET, TEST_DATA_PATH, mode='eval')
-
-    init_op = tf.global_variables_initializer()
-    mon_sess = GetTrainingSession(model_train, gpu_mem_portion=gpu_mem_portion)
-    return model_train, model_eval, init_op, mon_sess
-
-
-def evaluate(sess, model, best_precision, n_batch=50):
-    start_time = time.time()
-    total_prediction, correct_prediction = 0, 0
-    for _ in xrange(n_batch):
-        (loss, predictions, truth, train_step) = sess.run(
-            [model.cost, model.predictions,
-             model.labels, model.global_step])
-
-    truth = np.argmax(truth, axis=1)
-    predictions = np.argmax(predictions, axis=1)
-    correct_prediction += np.sum(truth == predictions)
-    total_prediction += predictions.shape[0]
-
-    precision = 1.0 * correct_prediction / total_prediction
-    best_precision = max(precision, best_precision)
-    end_time = time.time()
-    print "eval time ", end_time - start_time
-    return best_precision
-
-
-def training(model_train, model_eval, mon_sess, num_step, lr_vals, mu_vals, clip_thresh_vals, init_op, display_interval, log_dir, test_interval, use_meta=True):
-  mon_sess.run(init_op)
-  loss_list = []
-  precision_list = []
-
-  if use_meta:
-    model_train.optimizer.assign_hyper_param_value(lr_vals, mu_vals, clip_thresh_vals)
-    relu_rate_list = []
-    for i in range(len(model_train.relu_output) ):
-      relu_rate_list.append( [] )
-
-  iter_id = 0
-  best_precision = 0
-  while iter_id < num_step:
-      
-      if use_meta:
-        feed_dict = model_train.optimizer.get_hyper_feed_dict()
-        
-        output_results = mon_sess.run( [model_train.cost, model_train.grads, model_train.train_op, model_train.relu_output], feed_dict=feed_dict)
-      else:
-        output_results = mon_sess.run( [model_train.cost, model_train.grads, model_train.train_op] )
-
-      loss = output_results[0]
-      grad_vals = output_results[1]
-      loss_list.append(loss)
-      
-      if use_meta:
-        relu_output = output_results[3]
-        for i, act in enumerate(relu_output):
-            relu_rate_list[i].append(1.0 - (act[act < 0].size / float(act.size) ) )
-      
-      if use_meta:
-        model_train.optimizer.on_iter_finish(mon_sess, [ [g, ] for g in grad_vals], loss)
-        # model_train.optimizer.on_iter_finish( [ [g, ] for g in grad_vals], loss)
-
-      
-      if (iter_id % display_interval == 0 or iter_id==50 or iter_id == 250 or iter_id == 500 or iter_id == 1000 or iter_id == 2500) and iter_id != 0:
-          # evaluate bundle        
-          plt.figure()
-          plt.semilogy(loss_list, '.', alpha=0.2, label="Loss")
-          plt.semilogy(running_mean(loss_list,100), label="Average Loss")
-          plt.xlabel('Iterations')
-          plt.ylabel('Loss')
-          plt.legend()
-          plt.grid()
-          ax = plt.subplot(111)
-          ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
-                ncol=3, fancybox=True, shadow=True)
-          plt.savefig(log_dir + "/fig_loss_iter_" + str(iter_id) + ".png")
-          plt.close()
-          
-          if use_meta:    
-            # save relu rate
-            for i, relu_rate in enumerate(relu_rate_list):
-                with open(log_dir + "/relu_rate_" + str(i) + ".txt", "w") as f:
-                    np.savetxt(f, np.array(relu_rate) )
-            
-            model_train.optimizer.plot_curv(log_dir=log_dir)
-
-          with open(log_dir + "/loss.txt", "w") as f:
-            np.savetxt(f, np.array(loss_list) )
-          
-      if iter_id % test_interval == 0:
-          print "start test "
-          # do evaluation on whole test set
-          best_precision = evaluate(mon_sess, model_eval, best_precision, n_batch=50)
-          precision_list.append(best_precision)
-          print "best precision %.6f" % best_precision
-          
-          plt.figure()
-          plt.plot(test_interval * np.arange(len(precision_list) ), np.array(precision_list) )
-          plt.title("Test precision " + str(best_precision) )
-          plt.ylim( [0, 1] )
-          plt.savefig(log_dir + "/fig_acc.png")
-          plt.close()
-          
-          with open(log_dir + "/test_acc.txt", "w") as f:
-              np.savetxt(f, np.array(precision_list) )
-          
-      iter_id += 1
-        
-    
+def plot_loss(loss_list, log_dir, iter_id):
+    def running_mean(x, N):
+      cumsum = np.cumsum(np.insert(x, 0, 0)) 
+      return (cumsum[N:] - cumsum[:-N]) / N 
+    plt.figure()
+    plt.semilogy(loss_list, '.', alpha=0.2, label="Loss")
+    plt.semilogy(running_mean(loss_list,100), label="Average Loss")
+    plt.xlabel('Iterations')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid()
+    ax = plt.subplot(111)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+          ncol=3, fancybox=True, shadow=True)
+    plt.savefig(log_dir + "/fig_loss_iter_" + str(iter_id) + ".pdf")
+    print "figure plotted"
+    plt.close()
