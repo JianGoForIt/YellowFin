c8a6032ca56d446304f470d118103569b6f22a04
zjian <zjian@stanford.edu>
2017-06-11 16:14:49 -0700

    inline document for YF

--- tuner_utils/yellowfin.py
+++ tuner_utils/yellowfin.py
@@ -19,6 +19,18 @@ class YFOptimizer(object):
     delta_mu can be place holder/variable/python scalar. They are used for additional
     momentum in situations such as asynchronous-parallel training. The default is 0.0
     for basic usage of the optimizer.
+    Args:
+      lr: python scalar. The initial value of learning rate, we use 1.0 in our paper.
+      mu: python scalar. The initial value of momentum, we use 0.0 in our paper.
+      clip_thresh: python scalar. The cliping threshold for tf.clip_by_global_norm.
+        if None, no clipping will be carried out. 
+      beta: python scalar. The smoothing parameter for estimations.
+      delta_mu: for extensions. Not necessary in the basic use.
+    Other features:
+      If you want to manually control the learning rates, self.lr_factor is
+      an interface to the outside, it is an multiplier for the internal learning rate
+      in YellowFin. It is helpful when you want to do additional hand tuning
+      or some decaying scheme to the tuned learning rate in YellowFin.
     '''
     self._lr = lr
     self._mu = mu
