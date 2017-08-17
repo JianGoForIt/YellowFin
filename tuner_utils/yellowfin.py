5f7ee4cc5eba8d3349ad20477a5ffbbc331f2bd7
zjian <zjian@stanford.edu>
2017-08-17 10:31:51 -0700

    assertion for cubic root solver

--- tuner_utils/yellowfin.py
+++ tuner_utils/yellowfin.py
@@ -261,11 +261,19 @@ class YFOptimizer(object):
     # We use the Vieta's substution to compute the root.
     # There is only one real solution y (which is in [0, 1] ).
     # http://mathworld.wolfram.com/VietasSubstitution.html
-    p = self._dist_to_opt_avg**2 * self._h_min**2 / 2 / self._grad_var
-    w3 = (-tf.sqrt(p**2 + 4.0 / 27.0 * p**3) - p) / 2.0
-    w = tf.sign(w3) * tf.pow(tf.abs(w3), 1.0/3.0)
-    y = w - p / 3.0 / w
-    x = y + 1
+    assert_array = \
+      [tf.Assert(tf.logical_not(tf.is_nan(self._dist_to_opt_avg) ), [self._dist_to_opt_avg,]), 
+      tf.Assert(tf.logical_not(tf.is_nan(self._h_min) ), [self._h_min,]), 
+      tf.Assert(tf.logical_not(tf.is_nan(self._grad_var) ), [self._grad_var,]),
+      tf.Assert(tf.logical_not(tf.is_inf(self._dist_to_opt_avg) ), [self._dist_to_opt_avg,]), 
+      tf.Assert(tf.logical_not(tf.is_inf(self._h_min) ), [self._h_min,]), 
+      tf.Assert(tf.logical_not(tf.is_inf(self._grad_var) ), [self._grad_var,])]
+    with tf.control_dependencies(assert_array):
+      p = self._dist_to_opt_avg**2 * self._h_min**2 / 2 / self._grad_var
+      w3 = (-tf.sqrt(p**2 + 4.0 / 27.0 * p**3) - p) / 2.0
+      w = tf.sign(w3) * tf.pow(tf.abs(w3), 1.0/3.0)
+      y = w - p / 3.0 / w
+      x = y + 1
     return x
 
   def get_mu_tensor(self):
