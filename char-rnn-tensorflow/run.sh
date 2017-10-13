6e8549d007e145385d29f2021f552b80610c22f3
Jian Zhang <zhangjianthu@gmail.com>
2017-10-12 17:21:33 -0700

    parsing script for h_max log smoothing

--- char-rnn-tensorflow/run.sh
+++ char-rnn-tensorflow/run.sh
@@ -1,6 +1,6 @@
 for seed in 2 1 3
 do
-  python train_YF.py --log_dir=./results/YF_seed_${seed}_h_max_linear_h_min_log_lr_t_mu_t/ --data_dir=./data/tinyshakespeare/ --opt_method=YF --seed=${seed}
+  python train_YF.py --log_dir=./results/YF_seed_${seed}_h_max_linear_test_slow_start_10_win --data_dir=./data/tinyshakespeare/ --opt_method=YF --seed=${seed}
 
 #  python train.py --log_dir=./results/Adam_0.0005_seed_${seed}/ --data_dir=./data/tinyshakespeare/ --learning_rate=0.0005 --seed=${seed} --opt_method=Adam
 #  python train.py --log_dir=./results/Adam_0.001_seed_${seed}/ --data_dir=./data/tinyshakespeare/ --learning_rate=0.001 --seed=${seed} --opt_method=Adam
