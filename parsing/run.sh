b980ce9afbf1ec9629aa2232e4a94561521719d2
Jian Zhang <zhangjianthu@gmail.com>
2017-10-14 15:33:54 -0700

    run script for ptb parsing charrnn

--- parsing/run.sh
+++ parsing/run.sh
@@ -1,6 +1,6 @@
-for seed in 3 
+for seed in 2 1 3 
 do
-  mkdir -p models/wsj && python train.py --data_path=wsj --model_path=models/wsj/model --log_dir=./results/YF_seed_${seed}_h_max_log_test_slow_start_10_win --opt_method="YF" --seed=${seed} --h_max_log_smooth=1
+  mkdir -p models/wsj && python train.py --data_path=wsj --model_path=models/wsj/model --log_dir=../results/YF_seed_${seed}_h_max_log_test_slow_start_10_win_h_max_clip --opt_method="YF" --seed=${seed} --h_max_log_smooth=1
 done
 
 #for seed in 2 1 3
--- ptb/scripts/run.sh
+++ ptb/scripts/run.sh
@@ -1,6 +1,6 @@
 for seed in 2 1 3
 do
-  python PTB-release.py --seed=${seed} --opt_method=YF --log_dir=../results/YF_seed_${seed}_h_max_log_test_slow_start_10_win --h_max_log_smooth
+  python PTB-release.py --seed=${seed} --opt_method=YF --log_dir=../results/YF_seed_${seed}_h_max_log_test_slow_start_10_win_h_max_clip --h_max_log_smooth
 #  python PTB-release.py --seed=${seed} --opt_method=YF --log_dir=../results/YF_seed_${seed}_h_max_linear_h_min_log_lr_t_mu_t 
 done
 
